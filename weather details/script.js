7606a5f224fc74f384855cf75f04ec51cdf8054d
puredev <79652417+puredevcode@users.noreply.github.com>
2021-04-11 14:20:24 +0400

    Add files via upload

---
+++ weather details/script.js
@@ -0,0 +1,147 @@
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate =document.getElementById("climate");
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+
+searchButton.addEventListener('click', (e)=>
+{
+
+e.preventDefault();
+getWeather(searchInput.value);
+searchInput.value='';
+
+
+});
+
+
+
+const getWeather=async (city)=>
+{
+    try{
+
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dab3af44de7d24ae7ff86549334e45bd`,
+   
+            {mode: 'cors'}
+        );
+
+        const weatherData= await response.json();
+        console.log(weatherData);
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+        if(id<300 && id>200)
+        {
+            tempicon.src="./img/thunder.jpg"
+        }
+       else  if(id<400 && id>300)
+        {
+            tempicon.src="./img/sun-cloud.jpg"
+        }
+       else if(id<600&& id>500)
+        {
+            tempicon.src="./img/rain.jpg"
+        }
+       else  if(id<700 && id>600)
+        {
+            tempicon.src="./img/snow.png"
+        }
+       else  if(id<800 && id>700)
+        {
+            tempicon.src="./img/cloud.png"
+        }
+         else if(id==800)
+        {
+            tempicon.src="./img/sun.jpg"
+        }
+
+
+
+   
+    }
+catch(error)
+{
+    alert('city not found');
+}
+};
+
+
+
+
+
+
+
+
+
+window.addEventListener("load" ,()=>{
+
+let long;
+let lat;
+
+if(navigator.geolocation)
+{
+
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+
+   
+    
+    long=position.coords.longitude;
+    lat=position.coords.latitude;
+   
+
+        const api=`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=d3fd57748d05b8a35283a5454e50ce5b     `
+
+        fetch(api).then((response)=>{
+
+            return response.json();
+
+
+        })
+
+        .then (data =>
+            {
+
+                    const{name}=data;
+                    const{feels_like}=data.main;
+                    const{id,main}=data.weather[0];
+
+
+                    loc.textContent=name;
+                    climate.textContent=main;
+                    tempvalue.textContent=Math.round(feels_like-273);
+                    if(id<300 && id>200)
+                    {
+                        tempicon.src="./img/thunder.jpg"
+                    }
+                   else  if(id<400 && id>300)
+                    {
+                        tempicon.src="./img/sun-cloud.jpg"
+                    }
+                   else if(id<600&& id>500)
+                    {
+                        tempicon.src="./img/rain.jpg"
+                    }
+                   else  if(id<700 && id>600)
+                    {
+                        tempicon.src="./img/snow.png"
+                    }
+                   else  if(id<800 && id>700)
+                    {
+                        tempicon.src="./img/cloud.png"
+                    }
+                     else if(id==800)
+                    {
+                        tempicon.src="./img/sun.jpg"
+                    }
+
+                    console.log(data);
+            })
+}
+    )}
+})
